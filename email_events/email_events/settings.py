f96b0547d10b7211db0b0ed6701004b9c9d9c186
VikasDamodar <vikas9142@gmail.com>
2023-03-11 11:21:37 +0530

    Update settings.py

--- email_events/email_events/settings.py
+++ email_events/email_events/settings.py
@@ -18,8 +18,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-om!5ci)3*uf8mch-(z_ull6*m(oz4=vq^9&3v24x3-9ai@-)ae'
+# SECURITY WARNING: keep the secret key used in production secret! <Request for secret key>
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
